d6ee669e964926bd7e6297e86eb2622297eea862
Soumyadeep Adak <138775532+gitadak@users.noreply.github.com>
2024-05-23 18:50:22 +0530

    Update knapsack01.c

--- knapsack01.c
+++ knapsack01.c
@@ -3,18 +3,22 @@
 #define MAX_ITEMS 100
 #define MAX_WEIGHT 100
 
-int max(int a, int b) {
+int max(int a, int b)
+{
     return (a > b) ? a : b;
 }
 
-void knapsack(int weights[], int profits[], int n, int W) {
+void knapsack(int weights[], int profits[], int n, int W)
+{
     int i, w;
     int K[n+1][W+1];
     int selectedItems[MAX_ITEMS] = {0};
 
     // Build table K[][] in bottom up manner
-    for (i = 0; i <= n; i++) {
-        for (w = 0; w <= W; w++) {
+    for (i = 0; i <= n; i++)
+    {
+        for (w = 0; w <= W; w++)
+        {
             if (i == 0 || w == 0)
                 K[i][w] = 0;
             else if (weights[i-1] <= w)
@@ -23,42 +27,53 @@ void knapsack(int weights[], int profits[], int n, int W) {
                 K[i][w] = K[i-1][w];
         }
     }
-// Find items included in the knapsack
+
+    // Find items included in the knapsack
     int j = n, k = W;
-    while (j > 0 && k > 0) {
-        if (K[j][k] != K[j-1][k]) {
+    while (j > 0 && k > 0)
+    {
+        if (K[j][k] != K[j-1][k])
+        {
             selectedItems[j-1] = 1;
             k -= weights[j-1];
         }
         j--;
     }
-// Print total profit and selected items
+
+    // Print total profit and selected items
     printf("Total Profit: %d\n", K[n][W]);
     printf("Array specifying the Selected items(if selected then value at that index is 1 ,otherwise 0):\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         printf("%d ", selectedItems[i]);
     }
     printf("\n");
 }
 
-int main() {
+int main()
+{
     int n, W;
     int weights[MAX_ITEMS], profits[MAX_ITEMS];
-// Input number of items and weight of knapsack
+
+    // Input number of items and weight of knapsack
    printf("Enter the number of items: ");
     scanf("%d", &n);
     printf("Enter the weight of the knapsack: ");
     scanf("%d", &W);
-// Input weights and profits of items
+
+    // Input weights and profits of items
     printf("Enter the weights of the items:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &weights[i]);
     }
     printf("Enter the profits of the items:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &profits[i]);
     }
-// Solve knapsack problem
+
+    // Solve knapsack problem
     knapsack(weights, profits, n, W);
     return 0;
 }
